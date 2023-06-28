d2e95769620b51eca0196209bcaaea70a2406e6d
chongzhen <daybreakmu0@126.com>
2023-06-28 09:14:34 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace HelloWorld
+{
+    partial class MqttTestWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MqttTestWindow));
+            this.btConnect = new System.Windows.Forms.Button();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.btDisconnect = new System.Windows.Forms.Button();
+            this.tbIp = new System.Windows.Forms.TextBox();
+            this.lbIp = new System.Windows.Forms.Label();
+            this.lbPort = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nudTimeOut = new System.Windows.Forms.NumericUpDown();
+            this.btSubscripte = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txTopic1 = new System.Windows.Forms.TextBox();
+            this.txTopic2 = new System.Windows.Forms.TextBox();
+            this.btClearLog = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTimeOut)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btConnect
+            // 
+            resources.ApplyResources(this.btConnect, "btConnect");
+            this.btConnect.Name = "btConnect";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbLog
+            // 
+            resources.ApplyResources(this.tbLog, "tbLog");
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ReadOnly = true;
+            // 
+            // btDisconnect
+            // 
+            resources.ApplyResources(this.btDisconnect, "btDisconnect");
+            this.btDisconnect.Name = "btDisconnect";
+            this.btDisconnect.UseVisualStyleBackColor = true;
+            this.btDisconnect.Click += new System.EventHandler(this.btDisconnect_Click);
+            // 
+            // tbIp
+            // 
+            resources.ApplyResources(this.tbIp, "tbIp");
+            this.tbIp.Name = "tbIp";
+            // 
+            // lbIp
+            // 
+            resources.ApplyResources(this.lbIp, "lbIp");
+            this.lbIp.Name = "lbIp";
+            // 
+            // lbPort
+            // 
+            resources.ApplyResources(this.lbPort, "lbPort");
+            this.lbPort.Name = "lbPort";
+            // 
+            // tbPort
+            // 
+            resources.ApplyResources(this.tbPort, "tbPort");
+            this.tbPort.Name = "tbPort";
+            // 
+            // radioButton1
+            // 
+            resources.ApplyResources(this.radioButton1, "radioButton1");
+            this.radioButton1.Checked = true;
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            resources.ApplyResources(this.radioButton2, "radioButton2");
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            resources.ApplyResources(this.radioButton3, "radioButton3");
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // radioButton4
+            // 
+            resources.ApplyResources(this.radioButton4, "radioButton4");
+            this.radioButton4.Checked = true;
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            resources.ApplyResources(this.radioButton5, "radioButton5");
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.TabStop = true;
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            resources.ApplyResources(this.radioButton6, "radioButton6");
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.TabStop = true;
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // nudTimeOut
+            // 
+            resources.ApplyResources(this.nudTimeOut, "nudTimeOut");
+            this.nudTimeOut.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudTimeOut.Name = "nudTimeOut";
+            this.nudTimeOut.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // btSubscripte
+            // 
+            resources.ApplyResources(this.btSubscripte, "btSubscripte");
+            this.btSubscripte.Name = "btSubscripte";
+            this.btSubscripte.UseVisualStyleBackColor = true;
+            this.btSubscripte.Click += new System.EventHandler(this.btSubscripte_Click);
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // txTopic1
+            // 
+            resources.ApplyResources(this.txTopic1, "txTopic1");
+            this.txTopic1.Name = "txTopic1";
+            // 
+            // txTopic2
+            // 
+            resources.ApplyResources(this.txTopic2, "txTopic2");
+            this.txTopic2.Name = "txTopic2";
+            // 
+            // btClearLog
+            // 
+            resources.ApplyResources(this.btClearLog, "btClearLog");
+            this.btClearLog.Name = "btClearLog";
+            this.btClearLog.UseVisualStyleBackColor = true;
+            this.btClearLog.Click += new System.EventHandler(this.btClearLog_Click);
+            // 
+            // MqttTestWindow
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btClearLog);
+            this.Controls.Add(this.txTopic2);
+            this.Controls.Add(this.txTopic1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btSubscripte);
+            this.Controls.Add(this.nudTimeOut);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.lbPort);
+            this.Controls.Add(this.lbIp);
+            this.Controls.Add(this.tbIp);
+            this.Controls.Add(this.btDisconnect);
+            this.Controls.Add(this.tbLog);
+            this.Controls.Add(this.btConnect);
+            this.Name = "MqttTestWindow";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTimeOut)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.TextBox tbLog;
+        private System.Windows.Forms.Button btDisconnect;
+        private System.Windows.Forms.TextBox tbIp;
+        private System.Windows.Forms.Label lbIp;
+        private System.Windows.Forms.Label lbPort;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nudTimeOut;
+        private System.Windows.Forms.Button btSubscripte;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txTopic1;
+        private System.Windows.Forms.TextBox txTopic2;
+        private System.Windows.Forms.Button btClearLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Threading;
+using MQTTnet.Packets;
+using MQTTnet.Protocol;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    public partial class MqttTestWindow : Form
+    {
+        private IMqttClient _mqttClient;
+        public MqttTestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private MqttClientOptions GetClientOptions()
+        {
+            string ipValue = tbIp.Text.ToString();
+            int portValue = int.Parse(tbPort.Text.Trim().ToString());
+            if (ipValue == null || portValue == 0)
+            {
+                PrintLogs("ip端口号不能为空");
+                return null;
+            }
+            string clientId = Guid.NewGuid().ToString().Replace("-", "").ToUpper();
+            int secondValue = (int)nudTimeOut.Value;
+            TimeSpan timeOut = TimeSpan.FromSeconds(secondValue);
+            var options = new MqttClientOptionsBuilder()
+             .WithTcpServer(ipValue, portValue) // 设置Mqtt服务器的IP地址和端口号
+             .WithClientId(clientId) // 设置一个客户端ID，该ID必须唯一
+             .WithCleanSession()
+             .WithTimeout(timeOut)
+             .Build();
+            return options;
+        }
+
+        //连接客户端
+        private async void GetClientConnect()
+        {
+            _mqttClient = new MqttFactory().CreateMqttClient();
+            var options = GetClientOptions();
+            await _mqttClient.ConnectAsync(options, CancellationToken.None).ContinueWith(task =>
+           {
+               if (task.IsCompleted && !task.IsFaulted && !task.IsCanceled)
+               {
+                   PrintLogs("已连接到Mqtt服务器！");
+               }
+               else
+               {
+                   PrintLogs("连接Mqtt服务器失败！");
+               }
+           });
+        }
+
+        //断开客户端连接
+        public async void GetDisconnected()
+        {
+            await _mqttClient.DisconnectAsync();
+            PrintLogs("Mqtt客户端断开连接");
+        }
+
+        //Topic订阅
+        public async void SubscribeTopic()
+        {
+            string topic1 = txTopic1.Text.ToString().Trim();
+            string topic2 = txTopic2.Text.ToString().Trim();
+
+
+            var topicFilter1 = new MqttTopicFilterBuilder().WithTopic(topic1).WithQualityOfServiceLevel(GetQos()).Build();
+            var topicFilter2 = new MqttTopicFilterBuilder().WithTopic(topic2).WithQualityOfServiceLevel(GetQos()).Build();
+            var topicFilters = new List<MqttTopicFilter> { topicFilter1, topicFilter2 };
+
+            var subscribeOptions = new MqttClientSubscribeOptionsBuilder();
+            foreach (MqttTopicFilter topicFilter in topicFilters)
+            {
+                subscribeOptions.WithTopicFilter(topicFilter);
+            }
+            var options = subscribeOptions.
+             WithTopicFilter(topicFilter1).
+             WithTopicFilter(topicFilter2).Build();
+
+
+
+            await _mqttClient.SubscribeAsync(options);
+            
+            foreach (var topicFilter in topicFilters)
+            {
+                PrintLogs("Topic:" + topicFilter.Topic.ToString() + "订阅成功");
+            }
+
+            ReceiveMsg();
+
+        }
+        //接收订阅的消息
+        private void ReceiveMsg()
+        {
+            _mqttClient.ApplicationMessageReceivedAsync += delegate (MqttApplicationMessageReceivedEventArgs args) {
+                if (args.ApplicationMessage.Payload is null)
+                {
+                    PrintLogs("Empty message");
+                }
+                else
+                {
+                    PrintLogs(Encoding.UTF8.GetString(args.ApplicationMessage.Payload));
+                }
+
+                return System.Threading.Tasks.Task.CompletedTask;
+            };
+
+        }
+
+        private MqttQualityOfServiceLevel GetQos() 
+        {
+            var qosValue = new MqttQualityOfServiceLevel();
+            string value = ""; // 存储 RadioButton 的值
+            foreach (Control control in groupBox2.Controls)
+            {
+                if (control is RadioButton)
+                {
+                    RadioButton radioButton = (RadioButton)control;
+                    if (radioButton.Checked)
+                    {
+                        value = radioButton.Text;
+                        break;
+                    }
+                }
+            }
+            switch (value)
+            {
+                case "0":
+                    qosValue = MqttQualityOfServiceLevel.AtMostOnce; 
+                    break;
+                case "1":
+                    qosValue = MqttQualityOfServiceLevel.AtLeastOnce;
+                    break;
+                case "2":
+                    qosValue = MqttQualityOfServiceLevel.ExactlyOnce;
+                    break;
+            }
+            return qosValue;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string time = DateTime.Now.ToString("HH:mm:ss.fff",new CultureInfo("zh-cn"));
+            //tbLog.AppendText(time+"\r\n");
+            GetClientConnect();
+        }
+
+        private void btDisconnect_Click(object sender, EventArgs e)
+        {
+            GetDisconnected();
+        }
+
+        private void btClearLog_Click(object sender, EventArgs e)
+        {
+            tbLog.Clear();
+        }
+
+        private void PrintLogs(string content)
+        {
+            string time = DateTime.Now.ToString("HH:mm:ss.fff", new CultureInfo("zh-cn"));
+            tbLog.Invoke(new Action(() =>
+            {
+                tbLog.AppendText(time + ":  " + content + "\r\n");
+            }));
+            
+        }
+
+        private void btSubscripte_Click(object sender, EventArgs e)
+        {
+            SubscribeTopic();
+        }
+    }
+}
